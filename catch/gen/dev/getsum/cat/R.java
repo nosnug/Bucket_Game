c6ac9981ff3cf9f7f88520a0a4ed55b8c5c455da
nosnug <rawleur@gmail.com>
2013-08-21 23:06:43 -0700

    Removed dev.getsum.cat.util project. Gutted activity_game.xml file. Removed strings from strings.xml file.

--- catch/gen/dev/getsum/cat/R.java
+++ catch/gen/dev/getsum/cat/R.java
@@ -35,11 +35,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int dummy_button=0x7f090002;
-        public static final int fullscreen_content=0x7f090000;
-        public static final int fullscreen_content_controls=0x7f090001;
-        public static final int play_Button=0x7f090003;
+        public static final int action_settings=0x7f090001;
+        public static final int play_Button=0x7f090000;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -51,8 +48,6 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int dummy_button=0x7f060004;
-        public static final int dummy_content=0x7f060005;
         public static final int play_button_text=0x7f060002;
         public static final int title_activity_game=0x7f060003;
     }
